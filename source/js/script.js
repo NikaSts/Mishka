34550553a2273dd0065d4d41b73a3ab35cad6099
Nefila <veronika.sts@gmail.com>
2020-01-19 22:56:45 +0100

    длбавила валидацию формы и открытие/закрытие модалки

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,14 @@ var openButton = document.querySelector('.main-nav__open-button');
 var siteMenu = document.querySelector('.main-nav__list--site');
 var userMenu = document.querySelector('.main-nav__list--user');
 
+var modal = document.querySelector('.modal');
+var overlay = document.querySelector('.modal__overlay-js')
+var openModal = document.querySelectorAll('.modal-open-js');
+var addButton = document.querySelector('.modal-form__add-button');
+
+var form = document.querySelector('form');
+var sendButton = form.querySelector('.form__send-button');
+
 // открытие и закрытие мобильного меню
 
 siteMenu.classList.remove('main-nav__list--open');
@@ -9,11 +17,42 @@ userMenu.classList.remove('main-nav__list--open');
 siteMenu.classList.add('main-nav__list--close');
 userMenu.classList.add('main-nav__list--close');
 
-openButton.addEventListener('click', function () {
+openButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
   siteMenu.classList.toggle('main-nav__list--close');
   userMenu.classList.toggle('main-nav__list--close');
   siteMenu.classList.toggle('main-nav__list--open');
   userMenu.classList.toggle('main-nav__list--open');
 });
 
-// модальное окно
+// открытие и закрытие модального окна
+
+for (var i = 0; i < openModal.length; i++) {
+  openModal[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modal.classList.add('modal--open');
+    overlay.classList.add('modal--open');
+  });
+};
+
+addButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modal.classList.remove('modal--open');
+  overlay.classList.remove('modal--open');
+});
+
+overlay.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modal.classList.remove('modal--open');
+  overlay.classList.remove('modal--open');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modal.classList.contains('modal--open')) {
+      modal.classList.remove('modal--open');
+      overlay.classList.remove('modal--open');
+    }
+  }
+});
