32d5f720b1a00cc166ed2b5e3c609177fe356d11
Nefila <33237141+NikaSts@users.noreply.github.com>
2020-01-20 20:11:06 +0100

    Автоматизируем сборку личного проекта (#20)
    * правка сетки у форм
    
    * автоматизация - начало
    
    * сборка - продолжение
    
    * автоматизация - окончание
    
    * дополнения в gulp.watch
    
    * правки по замечаниям наставницы
    
    * длбавила валидацию формы и открытие/закрытие модалки
    
    * правки в svg
    
    * поправила svg
    
    * правки открытия и закрытия мобильного меню и ширины меню
    
    * правка паттерна телефона
    
    * добавление картинок webp, где не были добавлены
    
    * стилизация блока с картой
    
    * правка отступов
    
    * мелкие правки
    
    * удалила ненужные файлы
    
    * добавления условий для модалки
    
    * добавила решетку в src видео

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,60 @@
-var openButton = document.querySelector('.main-nav__open-button');
-var siteMenu = document.querySelector('.main-nav__list--site');
-var userMenu = document.querySelector('.main-nav__list--user');
+var mainNav = document.querySelector('.main-nav');
+var openButton = mainNav.querySelector('.main-nav__open-button');
+var mobileMenu = mainNav.querySelectorAll('.mobile-menu-open-js');
+
+
+// скрытие мобильного меню
+
+for (var i = 0; i < mobileMenu.length; i++) {
+  mobileMenu[i].classList.remove('main-nav__list--open');
+  mobileMenu[i].classList.add('main-nav__list--close');
+};
 
 // открытие и закрытие мобильного меню
 
-siteMenu.classList.remove('main-nav__list--open');
-userMenu.classList.remove('main-nav__list--open');
-siteMenu.classList.add('main-nav__list--close');
-userMenu.classList.add('main-nav__list--close');
-
-openButton.addEventListener('click', function () {
-  siteMenu.classList.toggle('main-nav__list--close');
-  userMenu.classList.toggle('main-nav__list--close');
-  siteMenu.classList.toggle('main-nav__list--open');
-  userMenu.classList.toggle('main-nav__list--open');
+openButton.addEventListener('click', function (evt) {
+  for (var i = 0; i < mobileMenu.length; i++) {
+    evt.preventDefault();
+    mobileMenu[i].classList.toggle('main-nav__list--close');
+    mobileMenu[i].classList.toggle('main-nav__list--open');
+  };
 });
 
-// модальное окно
+// открытие и закрытие модального окна
+
+if (modal) {
+  var addButton = modal.querySelector('.modal-form__add-button');
+  var modal = document.querySelector('.modal');
+  var overlay = document.querySelector('.modal__overlay-js')
+  var openModal = document.querySelectorAll('.modal-open-js');
+
+  for (var i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener('click', function (evt) {
+      evt.preventDefault();
+      modal.classList.add('modal--open');
+      overlay.classList.add('modal--open');
+    });
+  };
+
+  addButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modal.classList.remove('modal--open');
+    overlay.classList.remove('modal--open');
+  });
+
+  overlay.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modal.classList.remove('modal--open');
+    overlay.classList.remove('modal--open');
+  });
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (modal.classList.contains('modal--open')) {
+        modal.classList.remove('modal--open');
+        overlay.classList.remove('modal--open');
+      }
+    }
+  });
+};
